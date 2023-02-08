d4024733578530ea57b32024a05921034adec23e
AlessandroLembo <alessandrolembo2@gmail.com>
2023-02-08 21:49:50 +0100

    form style using bootstrap

--- functions.php
+++ functions.php
@@ -1,20 +1,16 @@
 <?php
 
-function rand_password() {
+function rand_password()
+{
     $message = '';
 
-    if(empty($_GET ['password_length'])){
+    if (empty($_GET['password_length'])) {
         $message = 'Inserisci la lunghezza della password';
     } else {
         $password_length = $_GET['password_length'];
         $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        $message = 'La lunghezza della password è: ' . substr(str_shuffle($chars), 0 ,$password_length);
+        $message = 'La password generata è: ' . substr(str_shuffle($chars), 0, $password_length);
     }
 
     return $message;
-
 }
-
- 
-?>   
-
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
 include 'functions.php';
-    
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,31 +15,48 @@ include 'functions.php';
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    
-    <!-- style CSS -->
-    <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
-    <div class="container ps-container">
-     <h1 class="text-center pt-5 ps-title-color">Strong Password Generator</h1>
-     <h2 class="text-center fs-2">Genera una password sicura</h2>
+    <div class="container bg-dark-subtle vh-100 p-5">
+        <h1 class="text-center pt-5">Strong Password Generator</h1>
+        <h2 class="text-center fs-2">Genera una password sicura</h2>
 
-    <div class="alert alert-info" role="alert">
-    <div><?= rand_password() ?></div>
+        <div class="alert alert-info my-5" role="alert">
+            <div class="fs-2"><?= rand_password() ?></div>
+        </div>
+
+        <form action="" method="GET" class="bg-warning-subtle py-5 px-3 rounded-2">
+            <div class="row mb-3 align-items-center">
+                <label for="inputEmail3" class="col-sm-8 col-form-label fs-3">Lunghezza Password:</label>
+                <div class="col-sm-2">
+                    <input type="number" class="form-control" id="inputEmail3" min="8" max="16" name="password_length">
+                </div>
+            </div>
+            <fieldset class="row mb-3">
+                <legend class="col-form-label col-sm-8 pt-0 fs-3">Consenti ripetizioni di uno o più caratteri:</legend>
+                <div class="col-sm-2">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1">
+                        <label class="form-check-label" for="gridRadios1">
+                            Si
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                        <label class="form-check-label" for="gridRadios2">
+                            No
+                        </label>
+                    </div>
+                </div>
+            </fieldset>
+
+            <button type="submit" class="btn btn-primary mt-5 px-4 py-2">Invia</button>
+
+        </form>
+
     </div>
-    
-    <form action="" method="GET">
-        <div class="d-flex align-items-center justify-content-around">
-           <label for="password">Lunghezza password</label>
-           <input type="number" min="8" max="16" name="password_length">
-        </div>   
-        
-    <button>Invia</button>
-        
-    </form>
-     
-
-</div>
 </body>
+
 </html>
